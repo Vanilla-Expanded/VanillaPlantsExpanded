1b9926f2a79c6988f955648f9a8a15c8f7e66027
Simon Latusek <Simon.Latusek@gmail.com>
2025-10-21 16:26:03 +0200

    made winterblooming configurable

---
+++ 1.6/Source/VanillaPlantsExpanded/VanillaPlantsExpanded/DefExtensions/WinterBloomingExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VanillaPlantsExpanded
+{
+    using Verse;
+
+    public class WinterBloomingExtension : DefModExtension
+    {
+        public GraphicData graphicSowing;
+        public GraphicData graphicWinter;
+    }
+}
--- 1.6/Source/VanillaPlantsExpanded/VanillaPlantsExpanded/Plant Classes/Plant_WinterBlooming.cs
+++ 1.6/Source/VanillaPlantsExpanded/VanillaPlantsExpanded/Plant Classes/Plant_WinterBlooming.cs
@@ -10,6 +10,7 @@ using Verse;
 
 namespace VanillaPlantsExpanded
 {
+
     [StaticConstructorOnStartup]
     public class Plant_WinterBlooming : Plant
     {
@@ -18,19 +19,33 @@ namespace VanillaPlantsExpanded
         private static Graphic GraphicWinter = GraphicDatabase.Get<Graphic_Random>("Things/Plant/Trees/VCE_TreeCherry_Blossomed", ShaderDatabase.CutoutPlant, Vector2.one, Color.white);
 
 
+        private bool?                   extensionPresent;
+        private WinterBloomingExtension defExtension;
+        public  WinterBloomingExtension DefExtension
+        {
+            get
+            {
+                if (this.extensionPresent.HasValue) 
+                    return this.extensionPresent.Value ? 
+                               this.defExtension : null;
+                this.extensionPresent = (this.defExtension = this.def.GetModExtension<WinterBloomingExtension>()) != null;
+                return this.defExtension;
+            }
+        }
+
         public override Graphic Graphic
         {
             get
             {
                 if (this.LifeStage == PlantLifeStage.Sowing)
                 {
-                    return GraphicSowing;
+                    return this.DefExtension?.graphicSowing?.Graphic ?? GraphicSowing;
                 }
                 Vector2 vector = Find.WorldGrid.LongLatOf(this.Map.Tile);
                 Season season = GenDate.Season((long)Find.TickManager.TicksAbs, vector);
                 if (season == Season.Winter)
                 {
-                    return GraphicWinter;
+                    return this.DefExtension?.graphicWinter?.Graphic ?? GraphicWinter;
                 }
                 if (this.def.plant.leaflessGraphic != null && this.LeaflessNow && (!this.sown || !this.HarvestableNow))
                 {
